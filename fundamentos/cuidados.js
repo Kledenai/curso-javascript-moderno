41f493160f9028e5e8292821aee0513eea0d9fd8
Khaneland <bruno.silva1643@etec.sp.gov.br>
2019-06-16 03:51:23 -0300

    alguns cuidados no js

---
+++ fundamentos/cuidados.js
@@ -0,0 +1,14 @@
+// Aqui vemos que ele vai imprimir um numero infinito
+console.log(7 / 0);
+
+// Dentro de uma string o js vai verificar se o valor pode ser atribuido na soma (não usa nunca caralho)
+console.log("10" / 2);
+
+// Vai da indefinido mas sabemos o porque né :v
+console.log("Show" * 2);
+
+// A especificação do js não é tão grande então ele não retornará 0.8
+console.log(0.1 + 0.7);
+
+// Essa é uma forma de poder aplicar uma função em numero quebrado quando já está sozinho, caso seja colocado na mão o valor
+console.log((10.345).toFixed(2));
