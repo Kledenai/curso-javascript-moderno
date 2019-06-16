00eddfab47ac9584fdb7bbef3222f2c8310ea058
Khaneland <bruno.silva1643@etec.sp.gov.br>
2019-06-16 03:33:13 -0300

    aula sobre tipagens, mostrando como o js tem uma tipagem fraca

---
+++ fundamentos/tipagem.js
@@ -0,0 +1,17 @@
+// Utilizando o typeof podemos ver o tipo de dado que vem na variavel
+
+let qualquer = 'legal';
+
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1546;
+
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evite nomes genericos e siglas
+
+const valor = '';
+const numero = 1;
+const pqp = false; // Puta que pariu... hue
