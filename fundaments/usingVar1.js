c68d7b1f83def0396f4da9cb51b64d8415a2597a
kledenai <bruno.silva1643@etec.sp.gov.br>
2021-07-16 23:50:44 -0300

    add lessons about using var in scope or blocks

---
+++ fundaments/usingVar1.js
@@ -0,0 +1,20 @@
+{
+  {
+    {
+      {
+        var willItBe = "Will it be???";
+        console.log(willItBe)
+      }
+    }
+  }
+}
+
+console.log(willItBe)
+
+function test() {
+  var local = 123
+  console.log(local)
+}
+
+test()
+// console.log(local) // var local is visible only within the function
---
+++ fundaments/usingVar2.js
@@ -0,0 +1,8 @@
+var number = 1
+
+{
+  var number = 2
+  console.log('inside =', number)
+}
+
+console.log('outside =', number)
