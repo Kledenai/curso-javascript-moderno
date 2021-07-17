995c0a8fa6304e70715737453c2a43f0f7387505
kledenai <bruno.silva1643@etec.sp.gov.br>
2021-07-16 23:35:31 -0300

    add lesson about arrow function and implicit return

---
+++ fundaments/function2.js
@@ -0,0 +1,18 @@
+// storing a function in the variable
+const printSum = function(a, b) {
+  console.log(a + b)
+}
+
+printSum(2, 5)
+
+// Storing a arrow function in the variable
+const sum = (a, b) => {
+  return a + b
+}
+
+console.log(sum(2,3))
+
+// implicit return
+const subtraction = (a,b) => a - b
+
+console.log(subtraction(3,2))
